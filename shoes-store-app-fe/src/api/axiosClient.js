342f1f3827e34967115057cf7bdb9ca16570b7c4
Le Thanh An <92533860+lean1123@users.noreply.github.com>
2025-01-03 22:53:37 +0700

    Update axiosClient.js

--- shoes-store-app-fe/src/api/axiosClient.js
+++ shoes-store-app-fe/src/api/axiosClient.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import AuthAPI from './authApi';
 
 const AdminAxiosClient = axios.create({
-	baseURL: 'http://18.181.186.230:8080/api',
-	// baseURL: 'http://localhost:8080/api',
+	//baseURL: 'http://18.181.186.230:8080/api',
+	baseURL: 'http://localhost:8080/api',
 	headers: {
 		'Content-Type': 'application/json',
 	},
