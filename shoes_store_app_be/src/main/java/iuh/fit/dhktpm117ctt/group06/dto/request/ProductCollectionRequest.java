211ebbfd99c4ef292f0847f617183725600ede8a
lean1123 <lethanhan20039@gmail.com>
2025-01-05 01:02:00 +0700

    handle product colleciton request

--- shoes_store_app_be/src/main/java/iuh/fit/dhktpm117ctt/group06/dto/request/ProductCollectionRequest.java
+++ shoes_store_app_be/src/main/java/iuh/fit/dhktpm117ctt/group06/dto/request/ProductCollectionRequest.java
@@ -2,9 +2,13 @@ package iuh.fit.dhktpm117ctt.group06.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 public class ProductCollectionRequest {
